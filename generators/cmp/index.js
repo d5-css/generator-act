68ee1d35662be749f14100506d7cd5f141cc04c3
csbun <icsbun@gmail.com>
2015-10-21 15:03:38 +0800

    update cmp and config

--- generators/cmp/index.js
+++ generators/cmp/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var path = require('path');
 var generators = require('yeoman-generator');
-var changeCase = require('change-case');
+var bower = require('bower');
+var chalk = require('chalk');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
     constructor: function () {
@@ -10,19 +13,63 @@ module.exports = generators.Base.extend({
 
         // This makes `subCommander` a required argument.
         this.argument('subCommander', { type: String, required: true });
-
         // This makes `componentName` a required argument.
         this.argument('componentName', { type: String, required: true });
-        // And you can then access it later on this way;
-        this.componentName = changeCase.paramCase(this.componentName);
     },
 
     // 创建文件结构
-    makeProjectDirectoryStructure: function () {
-        // 固定入口文件为 `components/${componentName}/index.${html|js|scss}`
-        var pageFileName = 'components/' + this.componentName + '/' + this.componentName;
-        this.copy('cmp.html', pageFileName + '.html');
-        this.copy('cmp.js', pageFileName + '.js');
-        this.template('_cmp.scss', pageFileName + '.scss');
+    makeComponent: function () {
+        switch (this.subCommander) {
+            // 初始化一个组件
+            case 'create':
+            case 'new':
+            case 'n':
+                // 固定入口文件为 `components/${componentName}/index.${html|js|less}`
+                this.componentName = _.kebabCase(this.componentName);
+                var componentPath = 'components/' + this.componentName;
+                var componentName = componentPath  + '/' + this.componentName;
+                this.copy('cmp.js', componentName + '.js');
+                this.template('_cmp.html', componentName + '.html');
+                this.template('_cmp.less', componentName + '.less');
+                // package.json 方便 require
+                this.template('_package.json', componentPath + '/package.json');
+                break;
+            // 安装一个组件
+            case 'install':
+            case 'i':
+                // 读取 package.json
+                var PACKAGE_JSON_PATH = './package.json';
+                var packageContent = {};
+                try {
+                    packageContent = this.fs.readJSON(PACKAGE_JSON_PATH);
+                } catch (e) {
+                    this.log(e.message);
+                }
+                var cmpnt = this.componentName;
+                // 读取 git 配置
+                var gitLocation = (packageContent.act || '').git || '';
+                if (cmpnt.indexOf('/') > 0 && gitLocation) {
+                    cmpnt = 'git@' + gitLocation + ':' + cmpnt;
+                }
+                // 通过 bower 下载
+                // tips
+                this.log(chalk.cyan('installing ') + cmpnt);
+                bower.commands
+                    .install([cmpnt], { save: true })
+                    .on('end', (function (installed) {
+                        var destinationRoot = this.destinationRoot();
+                        // set browserify alias
+                        if (!_.isObject(packageContent.browser)) {
+                            packageContent.browser = {};
+                        }
+                        _.forOwn(installed, function (val, key) {
+                            var mainFile = path.join(val.canonicalDir,(val.pkgMeta || '').main || 'index.js');
+                            packageContent.browser[key] = './' + path.relative(destinationRoot, mainFile).replace(/\\/g, '/');
+                        });
+                        // write package.json
+                        this.fs.writeJSON(PACKAGE_JSON_PATH, packageContent, null, 4);
+                    }).bind(this));
+                break;
+        }
     }
 });
